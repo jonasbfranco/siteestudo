5fea93fe316fd11399bab32d8169c48f868f85eb
Jonas <jonasbfranco@hotmail.com>
2020-05-14 22:50:44 -0300

    ...

---
+++ build/precache-manifest.ea9644632acf40cb9ed9d6f5c4c50e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4365f3ec1ff8a9ed5edd4dd3fc880edd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34df16e7a97f65a2f822",
+    "url": "/static/js/2.cef8905f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cef8905f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e23c3d2729649317cc5",
+    "url": "/static/js/main.41d41bf2.chunk.js"
+  },
+  {
+    "revision": "6390e12fb448e99e4bb7",
+    "url": "/static/js/runtime-main.b2f956ed.js"
+  },
+  {
+    "revision": "bf92eab6fcca88e8ab2a5b1a16940da7",
+    "url": "/static/media/foto_perfil.bf92eab6.png"
+  }
+]);
